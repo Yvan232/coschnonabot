d56064b133ef31a85161305b2d31f41bbf530823
y_y <50796543+Yvan232@users.noreply.github.com>
2021-12-02 21:18:51 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# coschnonabot
+# EthHunter
+Discord bot that uses the 2miners api to display mining data
---
+++ app.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const { MessageEmbed } = require("discord.js")
+
+const client = new Discord.Client();
+const prefix = '!';
+
+const trim = (str, max) => str.length > max ? `${str.slice(0, max - 3)}...` : str;
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', async message => {
+    if (message.content === 'cat') {
+        message.reply('schmock');
+      
+      
+		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+
+		message.channel.send(file);
+
+        //Es ist fucking 2 uhr wieso bau ich ne Katzen Funkiton ein was ist falsch in meinem leben 
+    }else if (message.content === '!status') {
+    
+        var statrespone = await fetch('https://api.corona-zahlen.org/states/').then (response => response.json());
+
+        const answer = statrespone;
+        const embed = new MessageEmbed()
+                .setColor('00ff94')
+                .setTitle('Aktuelle Daten')
+                .setImage('https://api.corona-zahlen.org/map/states-legend')
+                .addFields(
+                    {name: 'Inzidenz: ', value: statrespone.data.NW.weekIncidence},
+                    {name: 'Einwohner: ', value: statrespone.data.NW.population},
+                    {name: 'Erkrankungen: ', value: statrespone.data.NW.cases}
+                )
+        message.channel.send(embed);
+    }
+});
+
+client.login('OTEyODI2OTQ0MjgwNzM1NzY0.YZ1mVw.2v1VXJqHyhIj8H5TU3MdNjoTCGc');
