e5d040258e0ae11e8b9839835950b4f93338a53b
BuildTools <unconfigured@null.spigotmc.org>
2021-12-07 13:52:43 +0100

    removed CommandHandler

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const { MessageEmbed } = require("discord.js")
-
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
-
-
 const client = new Discord.Client();
 
 //replace with your token
